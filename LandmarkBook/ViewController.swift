9c3a46fe8d146c2b736fd566ff3266818633f1a5
Hamit Tırpan <hamittirpan@gmail.com>
2019-09-14 21:50:13 +0300

    first commit

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Xamarin Binding Context benzeri olan yapı
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // landmarkNames ve landmarkImages appendlerini aynı sırada koyuyorum.Çünkü diziler order'lı çalışır ve indexler.
+        // Landmark Book Data
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonehenge")
+        landmarkNames.append("Taj Mahal")
+        
+        
+        landmarkImages.append(UIImage(named: "colosseum")!)
+        landmarkImages.append(UIImage(named: "GreatWall")!)
+        landmarkImages.append(UIImage(named: "Kremlin")!)
+        landmarkImages.append(UIImage(named: "Stonehenge")!)
+        landmarkImages.append(UIImage(named: "TajMahal")!)
+    }
+    // Sola çekerek silme işlemi için kullanılan hazır func
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landmarkNames[indexPath.row]
+        return cell
+    }
+    
+    // TableView'ımda kaç tane row olacağını belirtiyorum
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNames.count
+    }
 }
 
